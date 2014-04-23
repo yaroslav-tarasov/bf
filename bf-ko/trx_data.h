2f9c9c5bda1bfc2f553c4cac55022e8d405b8281
yaroslav-tarasov <yaroslav.tarasov@gmail.com>
2014-04-23 12:07:41 +0400

    Альтернативаная структура правил

--- bf-ko/trx_data.h
+++ bf-ko/trx_data.h
@@ -29,6 +29,19 @@ typedef struct _ip_addr
 #endif
 } ip_addr_t;
 
+//  Как вариант базового правила
+//  но опять же нужны ли мак адреса? И сетевые маски?
+typedef struct {
+  u_int8_t smac[ETH_ALEN], dmac[ETH_ALEN]; /* Use '0' (zero-ed MAC address) for any MAC address.
+					      This is applied to both source and destination. */
+  u_int16_t vlan_id;                   /* Use '0' for any vlan */
+  u_int8_t  proto;                     /* Use 0 for 'any' protocol */
+  ip_addr   shost, dhost;              /* User '0' for any host. This is applied to both source and destination. */
+  ip_addr   shost_mask, dhost_mask;    /* IPv4/6 network mask */
+  u_int16_t sport_low, sport_high;     /* All ports between port_low...port_high means 'any' port */
+  u_int16_t dport_low, dport_high;     /* All ports between port_low...port_high means 'any' port */
+} filtering_rule_core_fields;
+
 typedef struct filter_rule_base {
 
 
