be26250c4c475a9f5b2274daaac7d687c761caec
yaroslav-tarasov <yaroslav.tarasov@gmail.com>
2014-05-05 11:36:37 +0400

    chain_rule

--- bf-ko/bf_config.h
+++ bf-ko/bf_config.h
@@ -11,20 +11,19 @@
  */
 
 struct  nf_bf_filter_config {
-    struct nf_hook_ops nfho_in;   //net filter hook option struct for input
-    struct nf_hook_ops nfho_out;  //net filter hook option struct for output
-
-    // struct nf__list list[1];
-    uint8_t    zero_rule[2]; // INPUT OUTPUT
+    struct nf_hook_ops      nfho_in;   //net filter hook option struct for input
+    struct nf_hook_ops      nfho_out;  //net filter hook option struct for output
     struct work_struct      work_logging;
     struct workqueue_struct *wq_logging;
-    struct sk_buff_head *skb_list;
+    struct sk_buff_head     *skb_list;
+    
+    uint8_t  chain_rule[2]; // Конечное правило для цепочек INPUT OUTPUT, по умолчанию ACCEPT,
+                            // цепочек фиксированное колличество, 2 штуки на входе и на выходе      
     atomic_t init;
     pid_t    pid_log;
 };
 
 typedef struct filter_rule_list {
-
     filter_rule_t fr;
     struct list_head full_list; /* kernel's list structure */
     struct list_head chain_list; /* kernel's list structure */
@@ -32,19 +31,20 @@ typedef struct filter_rule_list {
     struct rcu_head rcu;
 } filter_rule_list_t;
 
-void wfc(void);
+
 struct sock * get_nl_sock(void);
+void  wfc(void);
 pid_t get_client_pid(void);
-int nl_send_msg(struct sock * nl_sk,int destpid, int type,int flags,char* msg,int msg_size);
-int nl_send_lst(struct sock * nl_sk,int destpid,  filter_rule_list_t* lst,int lst_size,int* end_list);
+int   nl_send_msg(struct sock * nl_sk,int destpid, int type,int flags,char* msg,int msg_size);
+int   nl_send_lst(struct sock * nl_sk,int destpid,  filter_rule_list_t* lst,int lst_size,int* end_list);
 
 
-int find_rule(unsigned char* data);
-void add_rule(struct filter_rule* fr);
-void delete_rule(struct filter_rule* fr);
-int nl_send_msg(struct sock * nl_sk,int destpid, int type, int flags,char* msg,int msg_size);
-void list_rules(struct sock * nl_sk,int destpid);
-void delete_rules(void);
+int   find_rule(unsigned char* data);
+void  add_rule(struct filter_rule* fr);
+void  delete_rule(struct filter_rule* fr);
+int   nl_send_msg(struct sock * nl_sk,int destpid, int type, int flags,char* msg,int msg_size);
+void  list_rules(struct sock * nl_sk,int destpid);
+void  delete_rules(void);
 
 
 #endif // BF_CONFIG_H
