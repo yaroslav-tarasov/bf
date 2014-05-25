0881348a43000461b58e331620672f220a756af0
root <root@test.centos>
2014-05-25 20:43:03 +0400

    Little something

--- bf-link/bflocalcontrol.cpp
+++ bf-link/bflocalcontrol.cpp
@@ -1,6 +1,71 @@
 #include "bflocalcontrol.h"
 
+#include <QBuffer>
+#include <QDataStream>
+#include <QDateTime>
+
 BFLocalControl::BFLocalControl(QObject *parent) :
     QObject(parent)
 {
+    mLocalSocket = new QLocalSocket(this);
+}
+
+
+
+void BFLocalControl::init(const QString& serverName) {
+    mLocalSocket->connectToServer(serverName);
+    //TODO// m_SocketWantedData = 0;
+    connect(mLocalSocket, SIGNAL(connected()), SLOT(onConnected()));
+    connect(mLocalSocket, SIGNAL(readyRead()), SLOT(onReadyRead()));
+    connect(mLocalSocket, SIGNAL(disconnected()), SLOT(onDisconnected()));
+    connect(mLocalSocket, SIGNAL(error(QLocalSocket::LocalSocketError)), SLOT(onSocketError(QLocalSocket::LocalSocketError)));
+    // return 0;
+}
+
+
+void BFLocalControl::onConnected() {
+//    if(!m_CommandQueue.isEmpty()) {
+//        sendCommand(m_CommandQueue.takeFirst());
+//    }
+}
+
+void BFLocalControl::onReadyRead() {
+
+    if(mSocketWantedData <= 0) {
+        if(mLocalSocket->bytesAvailable() >= (qint64)sizeof(int)) {
+            mLocalSocket->read((char *)&mSocketWantedData, sizeof(int));
+        }
+    }
+
+    if(mSocketWantedData > 0 && mLocalSocket->bytesAvailable() >= mSocketWantedData) {
+        QByteArray ba = mLocalSocket->read(mSocketWantedData);
+        QBuffer bu(&ba);
+        bu.open(QIODevice::ReadOnly);
+        QDataStream s(&bu);
+
+//        RemoteDaemonResponse response;
+
+//        s >> response;
+
+
+//        if(m_SentCommands.contains(response.m_Sequence)) {
+//            emit commandResponse(m_SentCommands[response.m_Sequence], response);
+//        }
+        mSocketWantedData = 0;
+
+        if(mLocalSocket->bytesAvailable() > 0) {
+            onReadyRead();
+        }
+    }
+}
+
+void BFLocalControl::onDisconnected() {
+     mSocketWantedData = 0;
+}
+
+void BFLocalControl::onSocketError(QLocalSocket::LocalSocketError err) {
+    qWarning() << "WARNING: Socket error:"
+               << mLocalSocket->errorString() << "(" << err << ")";
+    mLocalSocket->disconnectFromServer();
+    mSocketWantedData = 0;
 }
--- bf-link/bflocalcontrol.h
+++ bf-link/bflocalcontrol.h
@@ -4,11 +4,14 @@
 #include <QObject>
 #include <QSharedPointer>
 #include <QList>
+#include <QLocalSocket>
+
 #include <memory>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 #include <linux/if_ether.h>
 #include <linux/netlink.h>
+
 #include "trx_data.h"
 #include "bf-link_global.h"
 
@@ -16,13 +19,25 @@ class BFLINKSHARED_EXPORT BFLocalControl : public QObject
 {
     Q_OBJECT
 public:
-    explicit BFLocalControl(QObject *parent = 0);
+
     typedef QSharedPointer <filter_rule_t > filter_rule_ptr;
 
+
+    explicit    BFLocalControl (QObject *parent = 0);
+    void        init           (const QString& serverName);
 signals:
     
 public slots:
-    
+
+private slots:
+    void onConnected();
+    void onReadyRead();
+    void onDisconnected();
+    void onSocketError(QLocalSocket::LocalSocketError err);
+
+private:
+    QLocalSocket *mLocalSocket;
+    int           mSocketWantedData;
 };
 
 #endif // BFLOCALCONTROL_H
