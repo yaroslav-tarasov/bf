a8c7faa7d611cf2a831d391c30dd6b51a6992002
yaroslav-tarasov <yaroslav.tarasov@gmail.com>
2014-06-11 12:09:39 +0400

    Мелочь, но надо привести в соответствие

--- bf-ko/do_test.sh
+++ bf-ko/do_test.sh
@@ -3,10 +3,10 @@
 make && {
 #if [ " lsmod | grep bf | awk `{print $1}`" -eq "bf_filter" ]; then
    echo "delete bf_filter"
-   rmmod bf_filter
+   rmmod bf-filter
 #if
 
-insmod bf_filter.ko 
+insmod bf-filter.ko 
 
 #lsmod 
 #dmesg
